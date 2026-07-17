a5e6f43a393bde94193490e73a9ecf0f2151e210
ethan-hann <36464732+ethan-hann@users.noreply.github.com>
2026-07-17 12:18:35 -0500

    add more functions and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,9 +9,25 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         
-        if not os.path.isdir(directory):
+        if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
-        else:
-            return f'Success: "{directory}" is within the working directory'
+        
+        file_records: list[dict[str, str]] = []
+        file_list = os.listdir(target_dir)
+        for file in file_list:
+            abs_path = os.path.normpath(os.path.join(target_dir, file))
+            file_records.append({
+                "name": os.path.basename(abs_path),
+                "size": os.path.getsize(abs_path),
+                "is_dir": os.path.isdir(abs_path)
+            })
+        
+        # Build return string
+        lines = [f"Result for {directory if directory != "." else "current directory"}:"]
+        for record in file_records:
+            lines.append(
+                f"  - {record["name"]}: file_size={record["size"]} bytes, is_dir={record["is_dir"]}"
+            )
+        return "\n".join(lines)
     except Exception as e:
-        return f'Error: {e}'
+        return f'Error: {str(e)}'
