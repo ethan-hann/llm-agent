8a5d528147757782d89c86e89f2cbe53d2a9af74
ethan-hann <36464732+ethan-hann@users.noreply.github.com>
2026-07-17 11:11:51 -0500

    Add verbose flag to arguments

--- main.py
+++ main.py
@@ -6,25 +6,32 @@ from openai import OpenAI
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 
+parser = argparse.ArgumentParser(description="BootAI - A simple command line AI chatbot")
+parser.add_argument("user_prompt", type=str, help="The prompt to send to the LLM")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
 def generate_content(client, messages):
     return client.chat.completions.create(model="openrouter/free", messages=messages)
 
-def print_response(response):
+def print_response(response, is_verbose):
     if not response.usage:
         raise RuntimeError("Response from AI was empty!")
     
-    prompt_tokens = response.usage.prompt_tokens
-    completion_tokens = response.usage.completion_tokens
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {completion_tokens}")
+    if is_verbose:
+        prompt_tokens = response.usage.prompt_tokens
+        completion_tokens = response.usage.completion_tokens
+        global args
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {completion_tokens}")
+    
     print(response.choices[0].message.content)
-
+    
 def main():
     if not api_key:
         raise RuntimeError("API Key was not found in environment.")
-    parser = argparse.ArgumentParser(description="BootAI - A simple command line AI chatbot")
-    parser.add_argument("user_prompt", type=str, help="The prompt to send to the LLM.")
-    args = parser.parse_args()
+    global args
     
     messages = [
         {"role": "user", "content": args.user_prompt},
@@ -32,7 +39,7 @@ def main():
     
     client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key,)
     response = generate_content(client, messages)
-    print_response(response)
+    print_response(response, args.verbose)
     
 if __name__ == "__main__":
     main()
